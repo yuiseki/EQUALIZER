e99e074459cdc9b132db36a39dac586aac7ddec7
yuiseki <yuiseki@gmail.com>
2023-06-03 14:46:02 +0900

    refine prompts

--- src/utils/langchain/chains/surface/prompt.ts
+++ src/utils/langchain/chains/surface/prompt.ts
@@ -15,15 +15,15 @@ Agenda:
 “How far do you think personalization of AI assistants like ChatGPT to align with a user's tastes and preferences should go? What boundaries, if any, should exist in this process?”
 
 Opinions of other participants:
-Opinions of supporters:
+Opinions from advocates:
 - パーソナライズされたAIアシスタントは、ユーザーにとってより価値のある情報やサービスを提供できる。
 - ユーザーの好みに合わせた体験は、満足度を向上させ、長期的な利用促進につながる。
 - パーソナライズによって、AIアシスタントはユーザーのニーズを正確に理解し、より効果的にサポートできる。
-Opinions of objectors:
+Opinions from objectors:
 - パーソナライズによって、ユーザーの情報が収集され、プライバシーが侵害される可能性がある。
 - パーソナライズはフィルターバブルを作り出し、情報の偏りをもたらす恐れがある。
 - パーソナライズによって、異なる意見や視点へのアクセスが制限される可能性がある。
-Opinions of neutralists:
+Opinions from neutralists:
 - パーソナライズはユーザーエクスペリエンスを向上させる一方、個人情報やプライバシーの保護には慎重に取り組む必要がある。
 - パーソナライズはユーザーのニーズに合わせた効果的なサービス提供を可能にするが、情報の偏りやフィルターバブルのリスクも認識されるべきである。
 - 境界線は、個人情報の適切な保護、透明性と選択肢の提供、利用者のコントロールといった要素に基づいて設定されるべきである。
