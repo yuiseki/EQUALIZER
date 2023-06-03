321186f74b2012fde5231be931abb6aec10256e5
yuiseki <yuiseki@gmail.com>
2023-06-03 12:42:40 +0900

    greetings as japanese 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 
 import styles from "./page.module.css";
 
-const greetings = `こんにちは！私は対話型熟議促進人類包摂支援システム「イコライザー」です。重要な熟議に協力していただけませんか？`;
+const greetings = `こんにちは！私は対話型熟議促進人類包摂支援システム「イコライザー」です。重要な議論に、ぜひ参加していただけませんか？`;
 
 export default function Home() {
   const [requesting, setRequesting] = useState(false);
