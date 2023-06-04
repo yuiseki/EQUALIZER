c9f683896bf9900466f82d34cd3c00d76621dc38
yuiseki <yuiseki@gmail.com>
2023-06-04 12:12:35 +0900

    polis互換スキーマ追加

---
+++ src/app/api/public/comments/route.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  const count = await prisma.comment.count();
+
+  return NextResponse.json({
+    count: count,
+  });
+}
