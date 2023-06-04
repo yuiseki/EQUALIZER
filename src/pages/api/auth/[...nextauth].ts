c80ad7a8b80b63907658411ba596e0726257035e
yuiseki <yuiseki@gmail.com>
2023-06-04 11:19:05 +0900

    とりあえずTwitter認証を実装

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_API_KEY as string,
