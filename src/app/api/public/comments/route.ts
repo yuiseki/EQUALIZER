ff6873543566ca77dc46537af6958995169807f7
yuiseki <yuiseki@gmail.com>
2023-06-04 19:04:13 +0900

    getServerSessionを挟む

--- src/app/api/public/comments/route.ts
+++ src/app/api/public/comments/route.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    console.warn("Not Authorized")
+  }
   const count = await prisma.comment.count();
   const results = await prisma.comment.findMany({
     select: {
