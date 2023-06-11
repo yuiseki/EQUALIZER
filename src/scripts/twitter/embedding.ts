48ad6536ce522ff85e5b40b9ed0480562ada60c6
yuiseki <yuiseki@gmail.com>
2023-06-11 14:58:59 +0900

    loadGenerateCommentsChain

---
+++ src/scripts/twitter/embedding.ts
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { Document } from "langchain/document";
+
+import fs from "node:fs/promises";
+import { parse } from "csv-parse/sync";
+
+const data = await fs.readFile(
+  "tmp/twitter/yuiseki/twilog/yuiseki_-230611.csv",
+  "utf-8"
+);
+const records = parse(data);
+const docs = [];
+for (const record of records) {
+  const tweetId = record[0];
+  // date
+  const tweetedAtDate = "20" + record[1].split(" ")[0];
+  const tweetedAtYear = tweetedAtDate.slice(0, 4);
+  const tweetedAtMonth = tweetedAtDate.slice(4, 6);
+  const tweetedAtDay = tweetedAtDate.slice(6, 8);
+  // time
+  const tweetedAtTime = record[1].split(" ")[1];
+  const tweetedAtHour = tweetedAtTime.slice(0, 2);
+  const tweetedAtMinutes = tweetedAtTime.slice(2, 4);
+  const tweetedAtSeconds = tweetedAtTime.slice(4, 6);
+  const dateTimeStr = `${tweetedAtYear}-${tweetedAtMonth}-${tweetedAtDay}T${tweetedAtHour}:${tweetedAtMinutes}:${tweetedAtSeconds}.000Z`;
+  const tweetedAt = new Date(dateTimeStr);
+  console.log(tweetedAt.toString());
+  const tweetText = record[2];
+  console.log(tweetText);
+  const doc = new Document({
+    pageContent: tweetText,
+    metadata: {
+      id: tweetId,
+      screenName: "yuiseki_",
+      tweetedAt: tweetedAt.getTime(),
+    },
+  });
+  docs.push(doc);
+}
+
+console.log(docs.length);
+
+const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+const directory = "tmp/twitter/yuiseki/vectorstores";
+await vectorStore.save(directory);
---
+++ tests/twitter/generateComments.ts
@@ -0,0 +1,50 @@
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import * as dotenv from "dotenv";
+import { PromptTemplate } from "langchain/prompts";
+import { BaseLanguageModel } from "langchain/dist/base_language";
+import { LLMChain } from "langchain/chains";
+import { OpenAI } from "langchain/llms/openai";
+dotenv.config();
+
+const directory = "tmp/twitter/yuiseki/vectorstores";
+const vectorStore = await HNSWLib.load(directory, new OpenAIEmbeddings());
+
+const questions = [
+  "ChatGPTのようなAIアシスタントの、ユーザーの趣味嗜好に合わせたパーソナライゼーションはどこまでやるべきだと思うか？",
+  "通勤時間はサービス残業であると思うか？",
+];
+
+for await (const question of questions) {
+  console.log("");
+  console.log("----- ----- ----- -----");
+  console.log("Q:", question);
+  console.log("");
+  const docs = await vectorStore.similaritySearch(question, 4);
+  console.log("Tweets:\n", docs.map((d) => d.pageContent).join("\n"));
+  console.log("");
+  const GENERATE_COMMENTS_PROMPT = /*#__PURE__*/ new PromptTemplate({
+    template:
+      "Use the following pieces of context to generate list of opinions for the question at the end.\n\n{context}\n\nQuestion: {question}\nOpinions:",
+    inputVariables: ["context", "question"],
+  });
+  const loadGenerateCommentsChain = ({
+    llm,
+  }: {
+    llm: BaseLanguageModel;
+  }): LLMChain => {
+    const chain = new LLMChain({
+      llm: llm,
+      prompt: GENERATE_COMMENTS_PROMPT,
+    });
+    return chain;
+  };
+  const llm = new OpenAI({ temperature: 0, maxTokens: 2000 });
+  const chain = loadGenerateCommentsChain({ llm });
+  const result = await chain.call({
+    context: docs.map((d) => d.pageContent).join("\n"),
+    question: question,
+  });
+  console.log("Opinions:", result.text);
+  console.log("----- ----- ----- -----");
+}
